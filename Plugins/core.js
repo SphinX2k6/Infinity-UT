5b054deab36360710104001b2134bb58da68e179
Aritra Mondal <134761750+SphinX2k6@users.noreply.github.com>
2023-07-22 13:32:43 +0530

    Update core.js

--- Plugins/core.js
+++ Plugins/core.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const axios = require("axios");
+
 const path = require("path");
 
 let mergedCommands = [
@@ -7,46 +8,17 @@ let mergedCommands = [
   "help",
   "h",
   "menu",
-  "support",
-  "supportgc",
 ];
 
 module.exports = {
   name: "others",
   alias: [...mergedCommands],
-  cooldown: 60 * 1000,
   uniquecommands: ["support", "creator"],
   description: "All miscleaneous commands",
   start: async (Infinity, m, { pushName, prefix, inputCMD, doReact }) => {
 
     
     switch (inputCMD) {
-/*      case "script":
-      case "sc":
-        await doReact("🧣");
-        let repoInfo = await axios.get(
-          "https://api.github.com/repos/FantoX001/Infinity-MD"
-        );
-        let repo = repoInfo.data;
-        let txt = `            🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${
-          repo.forks_count
-        }\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${
-          repo.license.name
-        }\n*📁 Repo Size:* ${(repo.size / 1024).toFixed(
-          2
-        )} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${
-          repo.html_url
-        }\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless hardwork by *Team Infinity*. ❞\n\n*©️ Team Infinity- 2023*`;
-        Infinity.sendMessage(m.from, { image: pic, caption: txt }, { quoted: m });
-        break;
-
-      case "support":
-      case "supportgc":
-        
-        let txt2 = `              🧣 *Support Group* 🧣\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help inside *Group*.\n\n*Thanks for using Infinity.*`;
-        Infinity.sendMessage(m.from, { image: pic, caption: txt2 }, { quoted: m });
-        break;*/
-
     case "creator":
           await doReact("💫")
 let pp = await Infinity.profilePictureUrl('917044585369@s.whatsapp.net', "image");
