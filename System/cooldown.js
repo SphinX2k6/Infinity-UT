d26f7d4fce43fbff6bd09fed68a5749bf69a90a4
Aritra Mondal <134761750+SphinX2k6@users.noreply.github.com>
2023-07-22 13:45:34 +0530

    Update cooldown.js

--- System/cooldown.js
+++ System/cooldown.js
@@ -1,8 +1,18 @@
-/**
- * @type { Map<String, moment.Moment> }
- */
-const cooldown = new Map()
-
-module.exports = {
-    cooldown,
+/* @by:Ari_Senpai 
+IG: https://www.instagram.com/ari_senpai_01/
+YT: https://www.youtube.com/@arisenpai_6595
+*/
+const commandCooldowns = {};
+function checkCooldown(command, cooldownTime) {
+  const currentTime = Date.now();
+  const lastExecutionTime = commandCooldowns[command] || 0;
+  if (currentTime - lastExecutionTime < cooldownTime) {
+    const remainingCooldown = lastExecutionTime + cooldownTime - currentTime;
+    return remainingCooldown;
+  }
+  commandCooldowns[command] = currentTime;
+  return 0; // No remaining cooldown time (command can be executed)
 }
+module.exports = {
+  checkCooldown,
+};
