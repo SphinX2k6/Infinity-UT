dc82b3c5d9b77286f944157ba95d854a2f0e9f6e
Aritra Mondal <134761750+SphinX2k6@users.noreply.github.com>
2023-07-22 13:36:52 +0530

    Update misc.js
    mcstat for minecraft freaks

--- Plugins/misc.js
+++ Plugins/misc.js
@@ -1,16 +1,15 @@
 const axios = require("axios");
 const fetch = require('node-fetch');
+const cooldownManager = require('../System/cooldown.js');
 
-let mergedCommands = [ 'why', 'advise',];
+let mergedCommands = ['why', 'advise', 'ping', 'mcstat'];
 
 module.exports = {
   name: "misc",
   alias: [...mergedCommands],
-  uniquecommands: [
-     'why',
-     'advise',
-  ],
-  description: "Commands to vanish bordom",
+  cooldown: 5000, // Default cooldown time in milliseconds (1 minute)
+  uniquecommands: ['why', 'advise', 'ping', 'mcstat'],
+  description: "Commands to vanish boredom",
   start: async (
     Infinity,
     m,
@@ -34,26 +33,50 @@ module.exports = {
       isAdmin,
     }
   ) => {
-      switch(inputCMD) {
-        case 'why': case 'Why': case 'why?':
-          const { data } = await axios.get('https:/nekos.life//api/v2/why')
-            m.reply('```'+data.why+'```')
-          break
+    if (cooldownManager.checkCooldown(inputCMD, module.exports.cooldown) > 0) {
+      return m.reply(`Command is on cooldown. Please wait ${module.exports.cooldown / 1000} seconds.`);
+    }
 
-        case 'advise':
-          await axios
-            .get(`https://api.adviceslip.com/advice`)
-            .then((response) => {
-                const text = '```Advise for you:'+response.data.slip.advice+'```'
-                m.reply(text)
-            })
-            .catch((err) => {
-                m.reply(`🔍 Error: ${err}`)
-            })
-          break
+    switch (inputCMD) {
+      case 'why':
+      case 'Why':
+      case 'why?':
+        const { data } = await axios.get('https://nekos.life/api/v2/why');
+        m.reply('```' + data.why + '```');
+        break;
 
-          
-      }
-    
+      case 'ping':
+        const start = new Date().getTime();
+        await m.reply("```Ping!```");
+        const end = new Date().getTime();
+        m.reply("*Pong!*\n ```" + (end - start) + "``` *ms*");
+        break;
+
+      case 'advise':
+        await axios
+          .get(`https://api.adviceslip.com/advice`)
+          .then((response) => {
+            const text = '```Advice for you:' + response.data.slip.advice + '```';
+            m.reply(text);
+          })
+          .catch((err) => {
+            m.reply(`🔍 Error: ${err}`);
+          });
+        break;
+
+      case 'mcstat':
+        const timeStart = Date.now();
+        //If you are a minecraft player // U can join our serevr .!
+        const infoip = (await axios.get(`https://api.mcsrvstat.us/2/bbn.one:30642`)).data;
+
+        m.reply(`======${(Date.now()) - timeStart}ms=====
+          🗺️Java: bbn.one:30642
+          🏳️Bedrock: bbn.one
+          🎊Port: 30642
+          🕋Server Status: ${infoip.online}
+          ⛱️Version: ${infoip.version}
+        `);
+        break;
+    }
   }
-}
+};
